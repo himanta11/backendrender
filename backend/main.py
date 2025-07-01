7a03db42a9565722adf67f3621f6867907edd943
himanta11 <himanta820@gmail.com>
2025-07-01 08:40:59 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ app.add_middleware(
         "https://dizloonai.vercel.app",
         "http://localhost",
         "http://localhost:3000",
-        "http://localhost:5000",
+        "http://localhost:5500",
         "http://127.0.0.1:5000",
         "http://127.0.0.1"
     ],
